d63425c8c7b81e44c7076f431b5a9b63fb0b1cea
User <feridquliyev137@gmail.com>
2022-01-17 14:49:51 +0400

    edit

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "../Todo/Todo.css"
-import {BsCircle} from 'react-icons/bs';
+import { BsCircle } from 'react-icons/bs';
 function Todo({ todo }) {
     return (
-        <div  key={todo.id} className='todo' >
+        <div key={todo.id} className='todo' >
             <div className='list'>
-                <p><BsCircle/></p>
+                <p><BsCircle /></p>
             </div>
             <div className='list' >
-               <h1>{todo.task}</h1>
+                <h1>{todo.task}</h1>
             </div>
         </div>
     )
